3ccb520439537f8e4b5e1eb6d2b9fb2506deaf16
leybraks <leybrak22@gmail.com>
2026-03-24 16:08:33 -0500

    hosts

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-w4(a1mtywe4rbgvqy65ja0q#_&s1im&ob+0e-4^y032uwb)9r^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '163.176.173.160', 'backend']
 
 
 # Application definition
