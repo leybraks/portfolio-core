5747846b1f376c2f9a3b41ec89dd759acb6377eb
leybraks <leybrak22@gmail.com>
2026-03-25 18:49:36 -0500

    fix: permitir dominio silvadata.me en Vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,4 +7,8 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
+  server: {
+    host: true, // Esto es vital para que Vite funcione dentro de Docker
+    allowedHosts: ['silvadata.me'] // La llave mágica para tu dominio
+  }
 })
